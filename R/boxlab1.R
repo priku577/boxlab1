1d44c6859c2fd94a7863a02ffd2341ea7a4368fe
boxi zhang <boxizhang@boxideMacBook-Pro.local>
2017-09-18 15:13:29 +0200

    ten1

--- R/boxlab1.R
+++ R/boxlab1.R
@@ -1,4 +1,4 @@
-#' Euclidian algorithm.
+#' @title Euclidian algorithm.
 #' @description This function uses dijkstra algorithm to find the shortest connection.
 #' @references from https://en.wikipedia.org/wiki/Euclidean_algorithm .
 #' @param x the first number.
@@ -28,6 +28,7 @@ euclidean <- function(x, y)
 #' @param v the start point.
 #' @export
 #' @examples
+#' data(wiki_graph)
 #' dijkstra()
 #' d <- dijkstra(cost=wiki_graph, v=1)
 dijkstra<-function(cost, v)
@@ -96,10 +97,10 @@ dijkstra<-function(cost, v)
 #'   \item v2 numeric vector
 #'   \item w numeric vector
 #' }
+#' @usage data(wiki_graph)
 #' @references \url{https://en.wikipedia.org/wiki/Graph}
 wiki_graph <-
 data.frame(v1=c(1,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,6),
 v2=c(2,3,6,1,3,4,1,2,4,6,2,3,5,4,6,1,3,5),
 w=c(7,9,14,7,10,15,9,10,11,2,15,11,6,6,9,14,2,9))
 
-save(wiki_graph,file='wiki_graph.RData')
--- tests/testthat/testthat.R
+++ tests/testthat/testthat.R
@@ -7,6 +7,8 @@ data.frame(v1=c(1,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,6),
 v2=c(2,3,6,1,3,4,1,2,4,6,2,3,5,4,6,1,3,5),
 w=c(7,9,14,7,10,15,9,10,11,2,15,11,6,6,9,14,2,9))
 
+context("dijkstra")
+
 test_that("outputs are correct in the Dijkstra algorithm.", {
     expect_equal(dijkstra(wiki_graph,1), c(0,7,9,20,20,11))
     expect_equal(dijkstra(wiki_graph,3), c(9,10,0,11,11,2))
